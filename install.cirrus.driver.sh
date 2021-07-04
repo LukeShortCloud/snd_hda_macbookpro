bf5be19a6b8a4a2e06db51f0f7d775b6c49235c0
Luke Short <ekultails@gmail.com>
2021-07-03 23:41:02 -0600

    Install for all kernels

--- install.cirrus.driver.sh
+++ install.cirrus.driver.sh
@@ -2,125 +2,130 @@
 
 # NOTA BENE - this script should be run as root
 
-# Fake the output of `uname` so the script will work in an Arch Linux chroot.
-# This assumes that only one 'linux*-headers' package is installed.
-uname() {if [[ "$1" == "-r" ]]; then echo $(ls -1 /usr/lib/modules/ | grep -P "^[0-9]+"); fi}
+for kernel in $(ls -1 /usr/lib/modules/ | grep -P "^[0-9]+"); do
+    pushd .
+    # Fake the output of `uname` so the script will work in an Arch Linux chroot.
+    # This assumes that only one 'linux*-headers' package is installed.
+    uname() {if [[ "$1" == "-r" ]]; then echo "${kernel}"; fi}
 
-kernel_version=$(uname -r | cut -d '-' -f1)  #ie 5.2.7
-major_version=$(echo $kernel_version | cut -d '.' -f1)
-minor_version=$(echo $kernel_version | cut -d '.' -f2)
-major_minor=${major_version}${minor_version}
+    kernel_version=$(uname -r | cut -d '-' -f1)  #ie 5.2.7
+    major_version=$(echo $kernel_version | cut -d '.' -f1)
+    minor_version=$(echo $kernel_version | cut -d '.' -f2)
+    major_minor=${major_version}${minor_version}
 
-revision=$(uname -r | cut -d '.' -f3)
-revpart1=$(echo $revision | cut -d '-' -f1)
-revpart2=$(echo $revision | cut -d '-' -f2)
-revpart3=$(echo $revision | cut -d '-' -f3)
+    revision=$(uname -r | cut -d '.' -f3)
+    revpart1=$(echo $revision | cut -d '-' -f1)
+    revpart2=$(echo $revision | cut -d '-' -f2)
+    revpart3=$(echo $revision | cut -d '-' -f3)
 
-build_dir='build'
-update_dir="/lib/modules/$(uname -r)/updates"
-patch_dir='snd_hda_macbookpro/patch_cirrus'
-hda_dir="$build_dir/hda-$kernel_version"
+    build_dir='build'
+    update_dir="/lib/modules/$(uname -r)/updates"
+    patch_dir='snd_hda_macbookpro/patch_cirrus'
+    hda_dir="$build_dir/hda-$kernel_version"
 
-[[ ! -d $update_dir ]] && mkdir $update_dir
-[[ ! -d $build_dir ]] && mkdir $build_dir
-[[ -d $hda_dir ]] && rm -rf $hda_dir
+    [[ ! -d $update_dir ]] && mkdir $update_dir
+    [[ ! -d $build_dir ]] && mkdir $build_dir
+    [[ -d $hda_dir ]] && rm -rf $hda_dir
 
-if [ -d /usr/src/linux-headers-$(uname -r) ]; then
-	# Debian Based Distro
-	:
-elif [ -d /usr/src/kernels/$(uname -r) ]; then
-	# Fedora Based Distro
-	:
-elif [ -d /usr/lib/modules/$(uname -r) ]; then
-	# Arch Linux Based Distro
-	:
-else
-	echo "Linux kernel headers not found."
-	echo "Debian (eg Ubuntu): /usr/src/linux-headers-$(uname -r)"
-	echo "Fedora: /usr/src/kernels/$(uname -r)"
-	echo "Arch Linux: /usr/lib/modules/$(uname -r)"
-	echo "assuming the linux kernel headers package is not installed"
-	echo "please install the appropriate linux kernel headers package:"
-	echo "Debian: sudo apt install linux-headers-$revpart3"
-	echo "Fedora: sudo dnf install kernel-headers"
-	echo "Arch Linux: sudo pacman -S linux-headers"
+    if [ -d /usr/src/linux-headers-$(uname -r) ]; then
+    	# Debian Based Distro
+    	:
+    elif [ -d /usr/src/kernels/$(uname -r) ]; then
+    	# Fedora Based Distro
+    	:
+    elif [ -d /usr/lib/modules/$(uname -r) ]; then
+    	# Arch Linux Based Distro
+    	:
+    else
+    	echo "Linux kernel headers not found."
+    	echo "Debian (eg Ubuntu): /usr/src/linux-headers-$(uname -r)"
+    	echo "Fedora: /usr/src/kernels/$(uname -r)"
+    	echo "Arch Linux: /usr/lib/modules/$(uname -r)"
+    	echo "assuming the linux kernel headers package is not installed"
+    	echo "please install the appropriate linux kernel headers package:"
+    	echo "Debian: sudo apt install linux-headers-$revpart3"
+    	echo "Fedora: sudo dnf install kernel-headers"
+    	echo "Arch Linux: sudo pacman -S linux-headers"
 
-	exit 1
+    	exit 1
 
-fi
+    fi
 
 
-# so for 5.4.0-48 version we need to use the patched Ubuntu source
-# and not the mainline kernel source
-# for the moment assume will be required for any further versions
-# what about 5.5, 5.6, 5.8 etc???
-if [ $major_version -eq 5 -a $minor_version -eq 4 -a $revpart2 -ge 48  -a `uname -v | grep -c Ubuntu` -eq 1 ]; then
+    # so for 5.4.0-48 version we need to use the patched Ubuntu source
+    # and not the mainline kernel source
+    # for the moment assume will be required for any further versions
+    # what about 5.5, 5.6, 5.8 etc???
+    if [ $major_version -eq 5 -a $minor_version -eq 4 -a $revpart2 -ge 48  -a `uname -v | grep -c Ubuntu` -eq 1 ]; then
 
-	# supposedly this should download to here
-	# well that fails - get the useless metadata package
-	# (remember this script should be run as root)
-	#apt source linux-source
+    	# supposedly this should download to here
+    	# well that fails - get the useless metadata package
+    	# (remember this script should be run as root)
+    	#apt source linux-source
 
-	if [ ! -d /usr/src/linux-source-$kernel_version ]; then
+    	if [ ! -d /usr/src/linux-source-$kernel_version ]; then
 
-		echo "linux kernel source not found in /usr/src: /usr/src/linux-source-$kernel_version"
-		echo "assuming the linux kernel source package is not installed"
-		echo "please install the linux kernel source package:"
-		echo "sudo apt install linux-source"
+    		echo "linux kernel source not found in /usr/src: /usr/src/linux-source-$kernel_version"
+    		echo "assuming the linux kernel source package is not installed"
+    		echo "please install the linux kernel source package:"
+    		echo "sudo apt install linux-source"
 
-		exit 1
-	
-	fi
+    		exit 1
+    	
+    	fi
 
-	tar --strip-components=3 -xvf /usr/src/linux-source-$kernel_version.tar.bz2 linux-source-$kernel_version/sound/pci/hda
+    	tar --strip-components=3 -xvf /usr/src/linux-source-$kernel_version.tar.bz2 linux-source-$kernel_version/sound/pci/hda
 
-else
+    else
 
-	# attempt to download linux-x.x.x.tar.xz kernel
-	wget -c https://cdn.kernel.org/pub/linux/kernel/v$major_version.x/linux-$kernel_version.tar.xz -P $build_dir
+    	# attempt to download linux-x.x.x.tar.xz kernel
+    	wget -c https://cdn.kernel.org/pub/linux/kernel/v$major_version.x/linux-$kernel_version.tar.xz -P $build_dir
 
-	if [[ $? -ne 0 ]]; then
-   		# if first attempt fails, attempt to download linux-x.x.tar.xz kernel
-   		kernel_version=$major_version.$minor_version
-   		wget -c https://cdn.kernel.org/pub/linux/kernel/v$major_version.x/linux-$kernel_version.tar.xz -P $build_dir
-	fi
+    	if [[ $? -ne 0 ]]; then
+       		# if first attempt fails, attempt to download linux-x.x.tar.xz kernel
+       		kernel_version=$major_version.$minor_version
+       		wget -c https://cdn.kernel.org/pub/linux/kernel/v$major_version.x/linux-$kernel_version.tar.xz -P $build_dir
+    	fi
 
-	[[ $? -ne 0 ]] && echo "kernel could not be downloaded...exiting" && exit
+    	[[ $? -ne 0 ]] && echo "kernel could not be downloaded...exiting" && exit
 
-	tar --strip-components=3 -xvf $build_dir/linux-$kernel_version.tar.xz linux-$kernel_version/sound/pci/hda --directory=build/
+    	tar --strip-components=3 -xvf $build_dir/linux-$kernel_version.tar.xz linux-$kernel_version/sound/pci/hda --directory=build/
 
-fi
+    fi
 
-mv hda $hda_dir
-mv $hda_dir/Makefile $hda_dir/Makefile.orig
-mv $hda_dir/patch_cirrus.c $hda_dir/patch_cirrus.c.orig
-cp $patch_dir/Makefile $patch_dir/patch_cirrus* $hda_dir/
-[[ $major_version == '4' ]] && sed -i 's/^#include <sound\/hda_codec.h>$/#include "hda_codec.h"/' $hda_dir/patch_cirrus.c
+    mv hda $hda_dir
+    mv $hda_dir/Makefile $hda_dir/Makefile.orig
+    mv $hda_dir/patch_cirrus.c $hda_dir/patch_cirrus.c.orig
+    cp $patch_dir/Makefile $patch_dir/patch_cirrus* $hda_dir/
+    [[ $major_version == '4' ]] && sed -i 's/^#include <sound\/hda_codec.h>$/#include "hda_codec.h"/' $hda_dir/patch_cirrus.c
 
-# as of 5.4.0-48 the 5.5 change has apparently been backported to 5.4 for Ubuntu and debian??
-[[ $major_version -eq 5 && $minor_version -eq 4 && $revpart2 -ge 48 ]] && sed -i 's/event = snd_hda_jack_tbl_get_from_tag(codec, tag);/event = snd_hda_jack_tbl_get_from_tag(codec, tag, 0);/' $hda_dir/patch_cirrus.c
+    # as of 5.4.0-48 the 5.5 change has apparently been backported to 5.4 for Ubuntu and debian??
+    [[ $major_version -eq 5 && $minor_version -eq 4 && $revpart2 -ge 48 ]] && sed -i 's/event = snd_hda_jack_tbl_get_from_tag(codec, tag);/event = snd_hda_jack_tbl_get_from_tag(codec, tag, 0);/' $hda_dir/patch_cirrus.c
 
-# if kernel version is >= 5.5 then change
-# event = snd_hda_jack_tbl_get_from_tag(codec, tag);
-# to
-# event = snd_hda_jack_tbl_get_from_tag(codec, tag, 0);
-[[ $major_version -eq 5 && $minor_version -ge 5 ]] && sed -i 's/event = snd_hda_jack_tbl_get_from_tag(codec, tag);/event = snd_hda_jack_tbl_get_from_tag(codec, tag, 0);/' $hda_dir/patch_cirrus.c
+    # if kernel version is >= 5.5 then change
+    # event = snd_hda_jack_tbl_get_from_tag(codec, tag);
+    # to
+    # event = snd_hda_jack_tbl_get_from_tag(codec, tag, 0);
+    [[ $major_version -eq 5 && $minor_version -ge 5 ]] && sed -i 's/event = snd_hda_jack_tbl_get_from_tag(codec, tag);/event = snd_hda_jack_tbl_get_from_tag(codec, tag, 0);/' $hda_dir/patch_cirrus.c
 
-# if kernel version >= 5.6 then
-# change timespec to timespec64
-# change getnstimeofday to ktime_get_real_ts64
-if [ $major_version -eq 5 -a $minor_version -ge 6 ]; then
-   sed -i 's/timespec/timespec64/' $hda_dir/patch_cirrus.c
-   sed -i 's/timespec/timespec64/' $hda_dir/patch_cirrus_new84.h
-   sed -i 's/getnstimeofday/ktime_get_real_ts64/' $hda_dir/patch_cirrus.c
-   sed -i 's/getnstimeofday/ktime_get_real_ts64/' $hda_dir/patch_cirrus_new84.h
-fi
+    # if kernel version >= 5.6 then
+    # change timespec to timespec64
+    # change getnstimeofday to ktime_get_real_ts64
+    if [ $major_version -eq 5 -a $minor_version -ge 6 ]; then
+       sed -i 's/timespec/timespec64/' $hda_dir/patch_cirrus.c
+       sed -i 's/timespec/timespec64/' $hda_dir/patch_cirrus_new84.h
+       sed -i 's/getnstimeofday/ktime_get_real_ts64/' $hda_dir/patch_cirrus.c
+       sed -i 's/getnstimeofday/ktime_get_real_ts64/' $hda_dir/patch_cirrus_new84.h
+    fi
 
-cd $hda_dir
+    cd $hda_dir
 
-KVER="$(uname -r)" make
+    KVER="$(uname -r)" make
 
-KVER="$(uname -r)" make install
+    KVER="$(uname -r)" make install
 
-echo -e "\ncontents of $update_dir"
-ls -lA $update_dir
+    echo -e "\ncontents of $update_dir"
+    ls -lA $update_dir
+    popd
+    rm -rf $build_dir
+done
